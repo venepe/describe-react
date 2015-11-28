85a6985e7519cc86d5d1cb84596feba1b03e1709
venepe <venepe@users.noreply.github.com>
2015-11-27 18:29:10 -0600

    Add strict

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let Colors = require('material-ui/lib/styles/colors');
 let ColorManipulator = require('material-ui/lib/utils/color-manipulator');
 let Spacing = require('material-ui/lib/styles/spacing');
